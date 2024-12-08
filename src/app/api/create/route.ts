13e47042a720312944a51b9b38333f51ee5e545f
jkm1990 <jeffkmanser@gmail.com>
2024-12-08 17:17:25 -0400

    tweaked import names in routes

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from 'next/server'
-import { createDoc } from '@/tools/DataManager';
+import { createTechnology } from '@/tools/DataManager';
 
 export function POST(request: NextRequest) {
-    return createDoc(request);
+    return createTechnology(request);
 }
