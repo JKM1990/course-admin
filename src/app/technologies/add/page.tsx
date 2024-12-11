311ba696dc3e75daddc23ddeb03f9fa0173066c3
jkm1990 <jeffkmanser@gmail.com>
2024-12-11 09:09:25 -0400

    added revalidate = 0 to add tech

--- src/app/technologies/add/page.tsx
+++ src/app/technologies/add/page.tsx
@@ -1,6 +1,8 @@
 import { getAllData } from "@/tools/DataManager";
 import AddTechnology from "./AddTechnology";
 
+export const revalidate = 0;
+
 export default async function AddTechnologyPage() {
   const { courses } = await getAllData();
   return <AddTechnology courses={courses} />;
